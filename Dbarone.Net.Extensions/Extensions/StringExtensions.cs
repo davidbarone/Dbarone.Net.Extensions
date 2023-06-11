035dad390eb59a9815fc73d1977455dd96e5ed79
davidbarone <davidbarone@live.com>
2023-06-11 17:20:38 +1000

    string Like() method.

--- Dbarone.Net.Extensions/Extensions/StringExtensions.cs
+++ Dbarone.Net.Extensions/Extensions/StringExtensions.cs
@@ -286,4 +286,147 @@ public static class StringExtensions
                 return str;
         }
     }
+
+    /// <summary>
+    /// Implementation of Like function similar to SQL Like.
+    /// Single wildcard character is '_' and multi-character wildcard is '%'.
+    /// Also supports [] characters to define character domains.
+    /// See: https://stackoverflow.com/questions/5417070/c-sharp-version-of-sql-like/8583383#8583383
+    /// </summary>
+    /// <param name="str">The input string.</param>
+    /// <param name="pattern">The pattern to test for.</param>
+    /// <returns>Returns true if the pattern matches the input string.</returns>
+    public static bool Like(this string str, string pattern)
+    {
+        bool isMatch = true,
+            isWildCardOn = false,
+            isCharWildCardOn = false,
+            isCharSetOn = false,
+            isNotCharSetOn = false,
+            endOfPattern = false;
+        int lastWildCard = -1;
+        int patternIndex = 0;
+        List<char> set = new List<char>();
+        char p = '\0';
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            char c = str[i];
+            endOfPattern = (patternIndex >= pattern.Length);
+            if (!endOfPattern)
+            {
+                p = pattern[patternIndex];
+
+                if (!isWildCardOn && p == '%')
+                {
+                    lastWildCard = patternIndex;
+                    isWildCardOn = true;
+                    while (patternIndex < pattern.Length &&
+                        pattern[patternIndex] == '%')
+                    {
+                        patternIndex++;
+                    }
+                    if (patternIndex >= pattern.Length) p = '\0';
+                    else p = pattern[patternIndex];
+                }
+                else if (p == '_')
+                {
+                    isCharWildCardOn = true;
+                    patternIndex++;
+                }
+                else if (p == '[')
+                {
+                    if (pattern[++patternIndex] == '^')
+                    {
+                        isNotCharSetOn = true;
+                        patternIndex++;
+                    }
+                    else isCharSetOn = true;
+
+                    set.Clear();
+                    if (pattern[patternIndex + 1] == '-' && pattern[patternIndex + 3] == ']')
+                    {
+                        char start = char.ToUpper(pattern[patternIndex]);
+                        patternIndex += 2;
+                        char end = char.ToUpper(pattern[patternIndex]);
+                        if (start <= end)
+                        {
+                            for (char ci = start; ci <= end; ci++)
+                            {
+                                set.Add(ci);
+                            }
+                        }
+                        patternIndex++;
+                    }
+
+                    while (patternIndex < pattern.Length &&
+                        pattern[patternIndex] != ']')
+                    {
+                        set.Add(pattern[patternIndex]);
+                        patternIndex++;
+                    }
+                    patternIndex++;
+                }
+            }
+
+            if (isWildCardOn)
+            {
+                if (char.ToUpper(c) == char.ToUpper(p))
+                {
+                    isWildCardOn = false;
+                    patternIndex++;
+                }
+            }
+            else if (isCharWildCardOn)
+            {
+                isCharWildCardOn = false;
+            }
+            else if (isCharSetOn || isNotCharSetOn)
+            {
+                bool charMatch = (set.Contains(char.ToUpper(c)));
+                if ((isNotCharSetOn && charMatch) || (isCharSetOn && !charMatch))
+                {
+                    if (lastWildCard >= 0) patternIndex = lastWildCard;
+                    else
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                isNotCharSetOn = isCharSetOn = false;
+            }
+            else
+            {
+                if (char.ToUpper(c) == char.ToUpper(p))
+                {
+                    patternIndex++;
+                }
+                else
+                {
+                    if (lastWildCard >= 0) patternIndex = lastWildCard;
+                    else
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                }
+            }
+        }
+        endOfPattern = (patternIndex >= pattern.Length);
+
+        if (isMatch && !endOfPattern)
+        {
+            bool isOnlyWildCards = true;
+            for (int i = patternIndex; i < pattern.Length; i++)
+            {
+                if (pattern[i] != '%')
+                {
+                    isOnlyWildCards = false;
+                    break;
+                }
+            }
+            if (isOnlyWildCards) endOfPattern = true;
+        }
+        return isMatch && endOfPattern;
+    }
 }
